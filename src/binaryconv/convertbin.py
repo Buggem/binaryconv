b177137a1cb77b601d42c5370aa0c41d56d974b9
Buggem <66670928+Buggem@users.noreply.github.com>
2022-08-22 18:49:26 +1000

    Support for help

--- src/binaryconv/convertbin.py
+++ src/binaryconv/convertbin.py
@@ -1,16 +1,28 @@
 import sys
 import subprocess
-def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+"""
+convertbin - the standard binaryconv module
+"""
+def text_to_bits(text, encoding=sys.getdefaultencoding(), errors='surrogatepass'):
+    """
+    Translates text to binary (base 2)
+    """
     bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+def text_from_bits(bits, encoding=sys.getdefaultencoding(), errors='surrogatepass'):
+    """
+    Translates base 2, or binary, to text
+    """
     n = int(bits, 2)
     return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
 def unicode_table(sr, er):
+    """
+    Unicode range from table specified. Example: unicode_table(0x0, 0x100).
+    This will provide the ASCII range.
+    Note that the ending character will not be included
+    """
     table = []
     for i in range(int(sr), int(er)):
         table.append(chr(i))
     return table
-def python_version(encoding=sys.getdefaultencoding()):
-    return (subprocess.run([sys.executable, "--version"], capture_output=True).stdout).decode(encoding)
